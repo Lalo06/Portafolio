ea132ca46168060563aaa4bff582e6c20259ac40
Eduardo <eduardo@netcommerce.mx>
2026-06-30 20:58:23 +0000

    Refactor

--- resources/views/notes.blade.php
+++ resources/views/notes.blade.php
@@ -1,23 +1,20 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>NOTES</title>
-</head>
-<body>
-    <h1>Escribe tus notas</h1>
-    <a href="{{ route('notes.create') }}">Crear Nota</a>
+@extends('layouts.app')
+@section('title', 'Notas')
+@section('content')
+<div class="flex flex-col items-center justify-center">
+    <h1 class="text-2xl font-bold text-black">Escribe tus notas</h1>
+    <a class="border-2 border-emerald-400 rounded-md text-black font-bold py-2 px-4 hover:bg-emerald-200" href="{{ route('notes.create') }}">Crear Nota</a>
 
-    <div>
-    <h1>Notas Guardadas</h1>
-    <ul>
+
+    <div class="flex flex-col items-center justify-center mt-4">
+    <h1 class="text-xl font-bold text-black">Notas Guardadas</h1>
+    <ul class="list-inside list-disc">
         @foreach ($notes as $note)
         <li>{{ $note->title }}</li>
         <li>{{ $note->content}}</li>
+        <li> Por: {{ $note->author}}</li>
         @endforeach
     </ul>
+    </div>
 </div>
-</body>
-</html>
+@endsection
--- resources/views/notes_create.blade.php
+++ resources/views/notes_create.blade.php
@@ -1,17 +1,17 @@
+@extends('layouts.app')
+<form class="w-full bg-emerald-100 p-6 rounded-md" action="{{ route('notes.store') }}" method="POST">
+    <div class="w-full flex flex-col items-center justify-center">
+    @csrf
+        <label class="ml-2 block text-md pl-2 pt-3" for="title">Titulo</label>
+        <input class="w-1/4 border rounded-md pl-2 ml-4" type="text" name="title" required>
 
-<div>
-    <form action="{{ route('notes.store') }}" method="POST">
-        @csrf
-        <label for="title">Titulo</label>
-        <input type="text" name="title" required>
+        <label class="ml-2 block text-md pl-2 pt-3" for="content">Contenido</label>
+        <textarea class="w-1/4 border rounded-md pl-2 ml-4" name="content" rows="5" required></textarea>
 
-        <label for="content">Contenido</label>
-        <textarea name="content" rows="5" required></textarea>
+        <label class="ml-2 block text-md pl-2 pt-3" for="author">Autor</label>
+        <input class="w-1/4 border rounded-md pl-2 ml-4" type="text" name="author" required>
 
-        <label for="author">Autor</label>
-        <input type="text" name="author" required>
+        <button class="bg-blue-600 hover:bg-blue-700 text-white font-bold py-2 px-4 ml-4 mt-4 rounded">Guardar Nota</button>
+    </div>
+</form>
 
-        <button>Guardar Nota</button>
-    </form>
-
-</div>
